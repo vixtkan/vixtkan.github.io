f9f5dc36d565a6696bfaf21af8a749cc68555595
vixtkan <n01717546@humber.ca>
2025-11-06 16:04:37 -0500

    Add files via upload

---
+++ HTML&JavaS/script.js
@@ -0,0 +1,21 @@
+
+function showHalloween() {
+  document.getElementById('halloween').style.display = 'block';
+}
+function showChristmas() {
+  document.getElementById('christmas').style.display = 'block';
+}
+function showNewYear() {
+  document.getElementById('newyear').style.display = 'block';
+}
+
+
+function hideHalloween() {
+  document.getElementById('halloween').style.display = 'none';
+}
+function hideChristmas() {
+  document.getElementById('christmas').style.display = 'none';
+}
+function hideNewYear() {
+  document.getElementById('newyear').style.display = 'none';
+}
